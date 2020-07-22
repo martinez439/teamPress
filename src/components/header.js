44c7563ff53161ebfc53225c2876f7616cf46d1e
Michael Martinez <michaelmartinez@Michaels-Air.fios-router.home>
2020-07-22 15:14:05 -0400

    navbar color and onclick

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,18 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import Image from "../images/sanatto.png"
+import { slide as Menu } from 'react-burger-menu'
+
+
+
+function handleClick(e) {
+  e.preventDefault();
+  let toggle = document.querySelector(".navbar-burger"); 
+  let menu = document.querySelector(".navbar-menu"); 
+  toggle.classList.toggle("is-active"); 
+  menu.classList.toggle("is-active");
+  console.log('The link was clicked.');
+}
 
 const Header = ({ siteTitle, menuLinks }) => (
   <nav className="navbar is-dark"
@@ -13,11 +25,17 @@ const Header = ({ siteTitle, menuLinks }) => (
       <div className="navbar-start">
       <a className="navbar-item"><img src={Image} width="132" height="28" alt="A dog smiling in a party hat" /></a> 
     </div>
-      <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="true">
+      <a onClick={handleClick} 
+      className="navbar-burger" 
+      aria-label="menu" 
+      aria-expanded="true">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
+      
       </a>
+
+   
     </div>
 
     <div className="navbar-menu">
@@ -26,7 +44,8 @@ const Header = ({ siteTitle, menuLinks }) => (
         flex: 1,
         flexDirection: 'row',
         justifyContent: 'flex-end',
-        alignItems: 'flex-end'
+        alignItems: 'flex-end',
+        color: 'black'
       }}>
       
           <nav className="breadcrumb">
@@ -37,10 +56,11 @@ const Header = ({ siteTitle, menuLinks }) => (
                   style={{
                     listStyleType: `none`,
                     padding: `.2rem`,
+                    fontWeight: `bold`
                     
                   }}
                 >
-                  <Link style={{ color: `white` }} to={link.link}>
+                  <Link style={{ color: `rgb(40, 180, 215` }} to={link.link}>
                     {link.name}
                   </Link>
                 </li>
